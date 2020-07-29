a58eb3a86b343ed7a5d4bded21c313549cc53d70
Max Streese <max.streese@smarttra.de>
2020-07-29 22:39:27 +0200

    Clean up the ConnectionHandler actor

--- src/main/scala/com/streese/akka/actors/ConnectionHandler.scala
+++ src/main/scala/com/streese/akka/actors/ConnectionHandler.scala
@@ -27,57 +27,37 @@ object ConnectionHandler {
 
     protected def behavior(sourceRef: ActorRef[Response]): Behavior[Request] = Behaviors.withTimers { timers =>
       timers.startTimerWithFixedDelay(Request.SinkMessage(TickRequest.Tick), 3.seconds)
-      Behaviors.receiveMessagePartial(handleCompletions orElse handleMessagesWhenStopped(sourceRef, defaultState))
+      behaviorWhenStopped(sourceRef, defaultState)
     }
 
-    private val handleCompletions: PartialFunction[Request, Behavior[Request]] = {
-        case Request.SinkCompleted    => Behaviors.stopped
-        case Request.SinkFailed(ex)   => Behaviors.stopped
-    }
-
-    private def handleMessagesWhenStopped(
+    private def behaviorWhenStopped(
       sourceRef: ActorRef[Response],
       state: State
-    ): PartialFunction[Request, Behavior[Request]] = {
+    ): Behavior[Request] = Behaviors.receiveMessagePartial {
       case Request.SinkMessage(msg) => msg match {
-        case TickRequest.Start =>
-          Behaviors.receiveMessagePartial(handleCompletions orElse handleMessagesWhenStarted(sourceRef, state))
-        case TickRequest.Stop =>
-          Behaviors.same
-        case TickRequest.Reset =>
-          Behaviors.receiveMessagePartial(handleCompletions orElse handleMessagesWhenStopped(sourceRef, defaultState)) 
-        case TickRequest.Tick =>
-          Behaviors.same
-        case TickRequest.Inc(inc) =>
-          Behaviors.receiveMessagePartial(
-            handleCompletions orElse handleMessagesWhenStopped(sourceRef, state.copy(inc = inc))
-          )
+        case TickRequest.Start    => behaviorWhenStarted(sourceRef, state)
+        case TickRequest.Stop     => Behaviors.same
+        case TickRequest.Reset    => behaviorWhenStopped(sourceRef, defaultState)
+        case TickRequest.Tick     => Behaviors.same
+        case TickRequest.Inc(inc) => behaviorWhenStopped(sourceRef, state.copy(inc = inc))
       }
+      case Request.SinkCompleted => Behaviors.stopped
+      case Request.SinkFailed(_) => Behaviors.stopped
     }
 
-    private def handleMessagesWhenStarted(
+    private def behaviorWhenStarted(
       sourceRef: ActorRef[Response],
       state: State
-    ): PartialFunction[Request, Behavior[Request]] = {
+    ): Behavior[Request] = Behaviors.receiveMessagePartial {
       case Request.SinkMessage(msg) => msg match {
-        case TickRequest.Start =>
-          Behaviors.same
-        case TickRequest.Stop =>
-          Behaviors.receiveMessagePartial(handleCompletions orElse handleMessagesWhenStopped(sourceRef, state))
-        case TickRequest.Reset =>
-          Behaviors.receiveMessagePartial(handleCompletions orElse handleMessagesWhenStarted(sourceRef, defaultState))  
-        case TickRequest.Tick =>
-          {
-            sourceRef ! Response.SourceMessage(TickResponse(state.n))
-            Behaviors.receiveMessagePartial(
-              handleCompletions orElse handleMessagesWhenStarted(sourceRef, state.applyInc())
-            )
-          }
-        case TickRequest.Inc(inc) =>
-          Behaviors.receiveMessagePartial(
-            handleCompletions orElse handleMessagesWhenStarted(sourceRef, state.copy(inc = inc))
-          )
+        case TickRequest.Start    => Behaviors.same
+        case TickRequest.Stop     => behaviorWhenStopped(sourceRef, state)
+        case TickRequest.Reset    => behaviorWhenStarted(sourceRef, defaultState)
+        case TickRequest.Tick     => {sourceRef ! Response.SourceMessage(TickResponse(state.n)); behaviorWhenStarted(sourceRef, state.applyInc())}
+        case TickRequest.Inc(inc) => behaviorWhenStarted(sourceRef, state.copy(inc = inc))
       }
+      case Request.SinkCompleted => Behaviors.stopped
+      case Request.SinkFailed(_) => Behaviors.stopped
     }
 
   }
